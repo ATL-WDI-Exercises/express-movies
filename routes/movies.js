f40f90a88b40dffead216dbe380490732c3a9115
Dr. Mike Hopper <drmikeh@gmail.com>
2016-08-17 12:01:23 -0400

    Movies Index Route is working.

---
+++ routes/movies.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var Movie = require('../models/movie');
+var router = express.Router();
+
+function makeError(res, message, status) {
+  res.statusCode = status;
+  var error = new Error(message);
+  error.status = status;
+  return error;
+}
+
+// INDEX
+router.get('/', function(req, res, next) {
+  let movieFilter = {};
+
+  if (req.query.genre) {
+    movieFilter.genre = req.query.genre;
+  }
+  if (req.query.year) {
+    movieFilter.year = req.query.year;
+  }
+
+  console.log('movieFilter:', movieFilter);
+  Movie.find(movieFilter)
+  .then(function(movies) {
+    res.render('movies/index', { movies: movies });
+  });
+});
+
+module.exports = router;
