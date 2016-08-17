5ee495461cf8ec6f20ea4cadaecd58dca658dab0
Dr. Mike Hopper <drmikeh@gmail.com>
2016-08-17 15:01:15 -0400

    Added some simple AJAX calls for demo purposes.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+let counter = 0;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', counter: counter });
+});
+
+router.get('/ajax-counter', function(req, res, next) {
+  counter += 1;
+  res.json({ counter: counter });
+});
+
+router.put('/ajax-counter', function(req, res, next) {
+  counter = Number(req.body.counter);
+  res.redirect('/');
 });
 
 module.exports = router;
